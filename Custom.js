8d9417442235ab4d84983050a9624e92c4ffaccd
Amitesh Ghosh <amieshghosh651995@gmail.com>
2021-05-16 20:29:13 +0530

    first commit

---
+++ Custom.js
@@ -0,0 +1,34 @@
+  function hideResumeDataSections()
+  { 
+    var Objective=document.getElementById("Objective");
+    var Highlights=document.getElementById("Highlights");
+    var Technical_Expertise=document.getElementById("Technical_Expertise");
+    var Oranization_Experience=document.getElementById("Oranization_Experience");
+    var Education_Qualification=document.getElementById("Education_Qualification");
+    var Technical_Certification=document.getElementById("Technical_Certification");
+    var Responsibilities=document.getElementById("Responsibilities");
+    var Extra=document.getElementById("Extra");
+    var Declaration=document.getElementById("Declaration");   
+
+    Objective.classList.toggle("hideResumeContent");
+    Highlights.classList.toggle("hideResumeContent");
+    Technical_Expertise.classList.toggle("hideResumeContent");
+    Oranization_Experience.classList.toggle("hideResumeContent");
+    Education_Qualification.classList.toggle("hideResumeContent");
+    Technical_Certification.classList.toggle("hideResumeContent");
+    Responsibilities.classList.toggle("hideResumeContent");
+    Extra.classList.toggle("hideResumeContent");
+    Personal_Information.classList.toggle("hideResumeContent");
+    Declaration.classList.toggle("hideResumeContent");
+  }
+
+function StickyNavigtion(header,sticky) {
+  if (window.pageYOffset > sticky)
+  {
+    header.classList.add("sticky");
+  } 
+  else 
+  {
+    header.classList.remove("sticky");
+  }
+}
